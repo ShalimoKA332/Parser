efa6abf84d251e35165f4e18fc9ad1c9c0af2010
zerol <kirillsalimo04@gmail.com>
2022-02-04 14:00:05 +0300

    Добавьте файлы проекта.

---
+++ Parser/GetRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace Parser
+{
+    class GetRequest
+    {
+        int count_uniq;
+        string ulr = @"https://www.simbirsoft.com/";
+        List<string> vs = new List<string>();
+        public void Requiest()
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(ulr);
+            string ts = htmlDoc.DocumentNode.InnerText;
+            string[] words = ts.Split(' ', ',', '.', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t');
+            Console.WriteLine(ts);
+
+            foreach (string s in words)
+            {
+               
+                if (s != "")
+                {
+                    vs.Add(s);
+                    if (vs.Contains(s))
+                        count_uniq++;
+                   
+                }
+            }
+
+        }
+        public void GetList()
+        {
+            foreach (string s in vs)
+            {
+               
+               
+             
+                Console.WriteLine(s + count_uniq);
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Parser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetRequest get = new GetRequest();
+            get.Requiest();
+            get.GetList();
+        }
+    }
+}
