639725bdbf5b084ea2cd20f3d7633d107d264c9c
zerol <kirillsalimo04@gmail.com>
2022-02-04 14:42:56 +0300

    Возможность парсить произвольные страницы

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -7,7 +7,7 @@ namespace Parser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Вступительное задание для SimbirSoft \nВыполнил Шалимо Кирилл Александрович\nВведите url ");
+            Console.WriteLine("Вступительное задание для SimbirSoft \nВыполнил: Шалимо Кирилл Александрович\nВведите url ");
             string url = Console.ReadLine();
             GetRequest get = new GetRequest();
             get.Requiest(url);
